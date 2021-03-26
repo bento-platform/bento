6296c74ecf5a8e6a96f8eea3490f40b9226698a6
Brennan Brouillette <brennan.brouillette.culliford@gmail.com>
2021-03-26 17:13:29 -0400

    optimizing web dockerfile

--- lib/web/startup.sh
+++ lib/web/startup.sh
@@ -1,6 +1,9 @@
+echo "-- Initializing BentoV2 Web Container.. --";
 rm -rf package-lock.json npm-shrinkwrap.json node_modules && 
     npm cache clean --force && 
     npm cache verify && 
     npm install --unsafe-perm=true --allow-root;
+echo "-- Building node.. --";
 npm run build;
+echo "-- Toggling nginx daemon : off --";
 nginx -g 'daemon off;'
