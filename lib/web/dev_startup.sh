6b451284a1819b0014c4add02bf319ff2bfbaa79
Brennan Brouillette <brennan.brouillette.culliford@gmail.com>
2021-04-16 10:35:45 -0400

    improved web build/startup (prod and dev)

--- lib/web/dev_startup.sh
+++ lib/web/dev_startup.sh
@@ -1,9 +1,7 @@
 echo "-- Initializing BentoV2 Web Container [ DEV ].. --";
-rm -rf package-lock.json npm-shrinkwrap.json node_modules && 
-    npm cache clean --force && 
-    npm cache verify && 
-    npm install --unsafe-perm=true --allow-root;
-echo "-- Building node.. --";
+
+echo "-- Running `npm run watch`.. --";
 npm run watch &
+
 echo "-- Toggling nginx daemon : off --";
 nginx -g 'daemon off;'
--- lib/web/startup.sh
+++ lib/web/startup.sh
@@ -1,9 +1,7 @@
 echo "-- Initializing BentoV2 Web Container [ PROD ].. --";
-rm -rf package-lock.json npm-shrinkwrap.json node_modules && 
-    npm cache clean --force && 
-    npm cache verify && 
-    npm install --unsafe-perm=true --allow-root;
+
 echo "-- Building node.. --";
 npm run build;
+
 echo "-- Toggling nginx daemon : off --";
 nginx -g 'daemon off;'
