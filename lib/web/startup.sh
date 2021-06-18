cf58a494f4af3eb1a5e4fdcdfbf1141df1573ca0
Brennan Brouillette <brennan.brouillette.culliford@gmail.com>
2021-06-18 15:26:20 -0400

    patched web

--- lib/web/startup.sh
+++ lib/web/startup.sh
@@ -1,7 +1,8 @@
 echo "-- Initializing BentoV2 Web Container [ PROD ].. --";
+cd bento_web;
 
 echo "-- Building node.. --";
-npm run build;
+npm run build -y;
 
 echo "-- Toggling nginx daemon : off --";
 nginx -g 'daemon off;'
