352467b2b329de63f88099344f327cc2ed33b1e9
Brennan Brouillette <brennan.brouillette.culliford@gmail.com>
2021-08-18 12:05:57 -0400

    better variant/drs startups

--- lib/drs/startup.sh
+++ lib/drs/startup.sh
@@ -4,3 +4,11 @@ mkdir obj;
 cd ../chord_drs;
 flask db upgrade;
 flask run --host=0.0.0.0;
+
+echo "sleeping for 5 seconds";
+sleep 5;
+
+# call variant endpoint to trigger init
+URL=$VARIANT_SERVICE_URL/private/post-start-hook
+echo "calling $URL";
+curl $URL
