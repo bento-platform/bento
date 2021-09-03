1914373d3379d85c7488922cce11429d351d236b
Brennan Brouillette <brennan.brouillette.culliford@gmail.com>
2021-09-03 12:32:36 -0400

    fixed gunicorn logging

--- lib/logging/gunicorn_starter.sh
+++ lib/logging/gunicorn_starter.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-gunicorn app -w 2 --threads 2 -b 0.0.0.0:${INTERNAL_PORT}
+
+# using 1 worker, multiple threads
+# see https://stackoverflow.com/questions/38425620/gunicorn-workers-and-threads
+gunicorn bento_log_service.app:application -w 1 --threads $(expr 2 \* $(nproc --all) + 1) -b 0.0.0.0:${INTERNAL_PORT}
