a18ac6a36c1b34514ac54f046b86f65086e75076
brouillette <brennan.brouillette@computationalgenomics.ca>
2021-09-08 14:28:31 -0400

    patched startup; db upgrade

--- lib/notification/startup.sh
+++ lib/notification/startup.sh
@@ -1,3 +1,6 @@
+cd bento_notification_service
 flask db upgrade
+
 # flask run --host=0.0.0.0
+cd ..
 gunicorn bento_notification_service.app:application -w 1 --threads $(expr 2 \* $(nproc --all) + 1) -b 0.0.0.0:${INTERNAL_PORT}
