3347d3f10704ae00e9657860c82b0689177adab8
Gordon Krieger <gordon.krieger@gmail.com>
2022-08-24 16:34:54 -0400

    fix beacon gunicorn script

--- lib/beacon/gunicorn_starter.sh
+++ lib/beacon/gunicorn_starter.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn bento_service_registry.app:application -w 1 --threads $(expr 2 \* $(nproc --all) + 1) -b 0.0.0.0:${BEACON_INTERNAL_PORT}
+gunicorn bento_beacon.app:app -w 1 --threads $(expr 2 \* $(nproc --all) + 1) -b 0.0.0.0:${BEACON_INTERNAL_PORT}
