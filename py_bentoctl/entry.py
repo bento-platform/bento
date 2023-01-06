da71dcbca90b0511a37b6a8341c5375a5ef64608
David Lougheed <david.lougheed@gmail.com>
2023-01-06 08:45:46 -0500

    work on shell functionality for bentoctl

--- py_bentoctl/entry.py
+++ py_bentoctl/entry.py
@@ -6,7 +6,7 @@ from abc import ABC, abstractmethod
 
 from .auth_helper import init_auth
 from .config import BENTO_DOCKER_SERVICES
-from .services import run_service, stop_service, clean_service
+from . import services as s
 
 from typing import Optional, Type
 
@@ -42,7 +42,7 @@ class Run(SubCommand):
 
     @staticmethod
     def exec(args):
-        run_service(args.service)
+        s.run_service(args.service)
 
 
 class Stop(SubCommand):
@@ -55,7 +55,7 @@ class Stop(SubCommand):
 
     @staticmethod
     def exec(args):
-        stop_service(args.service)
+        s.stop_service(args.service)
 
 
 class Clean(SubCommand):
@@ -68,7 +68,39 @@ class Clean(SubCommand):
 
     @staticmethod
     def exec(args):
-        clean_service(args.service)
+        s.clean_service(args.service)
+
+
+class Shell(SubCommand):
+
+    @staticmethod
+    def add_args(sp):
+        sp.add_argument(
+            "service", type=str, choices=BENTO_DOCKER_SERVICES,
+            help="Service to enter a shell for.")
+        sp.add_argument(
+            "--shell", "-s", default="/bin/bash", type=str, choices=("/bin/bash", "/bin/sh"),
+            help="Shell to use inside the service container.")
+
+    @staticmethod
+    def exec(args):
+        s.enter_shell_for_service(args.service, args.shell)
+
+
+class RunShell(SubCommand):
+
+    @staticmethod
+    def add_args(sp):
+        sp.add_argument(
+            "service", type=str, choices=BENTO_DOCKER_SERVICES,
+            help="Service to run a shell for.")
+        sp.add_argument(
+            "--shell", "-s", default="/bin/bash", type=str, choices=("/bin/bash", "/bin/sh"),
+            help="Shell to run inside the service container.")
+
+    @staticmethod
+    def exec(args):
+        s.run_as_shell_for_service(args.service, args.shell)
 
 
 def main(args: Optional[list[str]] = None) -> int:
@@ -90,7 +122,9 @@ def main(args: Optional[list[str]] = None) -> int:
     _add_subparser("init-auth", "Configure authentication for BentoV2 with a local Keycloak instance.", InitAuth)
     _add_subparser("run", "Run Bento services.", Run)
     _add_subparser("stop", "Stop Bento services.", Stop)
-    _add_subparser("stop", "Clean services.", Clean)
+    _add_subparser("clean", "Clean services.", Clean)
+    _add_subparser("shell", "Run a shell inside an already-running service container.", Shell)
+    _add_subparser("run-as-shell", "Run a shell inside a stopped service container.", RunShell)
 
     p_args = parser.parse_args(args)
 
--- py_bentoctl/services.py
+++ py_bentoctl/services.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 from termcolor import cprint
@@ -8,6 +9,8 @@ __all__ = [
     "run_service",
     "stop_service",
     "clean_service",
+    "enter_shell_for_service",
+    "run_as_shell_for_service",
 ]
 
 
@@ -52,3 +55,23 @@ def clean_service(service: str):
 
     print(f"Stopping {service}...")
     subprocess.check_call((*COMPOSE, "rm", "-svf", service))
+
+
+def enter_shell_for_service(service: str, shell: str):
+    if service not in BENTO_DOCKER_SERVICES:
+        cprint(f"  {service} not in list of services: {BENTO_DOCKER_SERVICES}")
+        exit(1)
+
+    cmd = COMPOSE[0]
+    compose_args = COMPOSE[1:]
+    os.execvp(cmd, (cmd, *compose_args, "exec", "-it", service, shell))  # TODO: Detect shell
+
+
+def run_as_shell_for_service(service: str, shell: str):
+    if service not in BENTO_DOCKER_SERVICES:
+        cprint(f"  {service} not in list of services: {BENTO_DOCKER_SERVICES}")
+        exit(1)
+
+    cmd = COMPOSE[0]
+    compose_args = COMPOSE[1:]
+    os.execvp(cmd, (cmd, *compose_args, "run", service, shell))  # TODO: Detect shell
