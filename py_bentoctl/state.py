5aa218eae2bfdae81ad007960d4c0feebd4742ee
David Lougheed <david.lougheed@gmail.com>
2023-01-16 10:55:11 -0500

    fix issues with state schema/initial data insert

--- py_bentoctl/state.py
+++ py_bentoctl/state.py
@@ -14,7 +14,10 @@ __all__ = [
 ]
 
 STATE_SCHEMA = """
-CREATE TABLE IF NOT EXISTS kvstore (k TEXT, v TEXT);
+CREATE TABLE IF NOT EXISTS kvstore (
+    k TEXT PRIMARY KEY, 
+    v TEXT
+);
 """
 
 
@@ -32,10 +35,8 @@ def get_state():
     try:
         with db:
             db.executescript(STATE_SCHEMA)
-            db.execute("""
-            INSERT INTO kvstore (k, v) VALUES ('services', ?) 
-            WHERE NOT EXISTS (SELECT 1 FROM kvstore WHERE k = 'services')
-            """, (json.dumps(first_run_services),))
+            db.execute("INSERT OR IGNORE INTO kvstore (k, v) VALUES ('services', ?)", (json.dumps(first_run_services),))
+            db.commit()
 
             rs = db.execute("SELECT v FROM kvstore WHERE k = 'services'")
             if (services_r := rs.fetchone()) is not None:
@@ -52,7 +53,7 @@ def set_state_services(services: Dict[str, Dict[str, Any]]):
     db = sqlite3.connect(BENTO_ORCHESTRATION_STATE_DB_FILE)
     try:
         with db:
-            db.execute("INSERT OR REPLACE INTO kvstore (k, v) VALUES ('services', ?)", json.dumps(services))
+            db.execute("INSERT OR REPLACE INTO kvstore (k, v) VALUES ('services', ?)", (json.dumps(services),))
             db.commit()
     finally:
         db.close()
