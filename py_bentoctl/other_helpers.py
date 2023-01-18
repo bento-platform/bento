3cb9df328ad56c0812664e2c7cc2ea35e60b3162
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2023-01-18 14:16:54 -0500

    openssl.py created

--- py_bentoctl/other_helpers.py
+++ py_bentoctl/other_helpers.py
@@ -1,13 +1,7 @@
-import datetime
 import os
 import pathlib
-import glob
 from termcolor import cprint
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography import x509
-from cryptography.x509.oid import NameOID
-from cryptography.hazmat.primitives import hashes
+from .openssl import _create_cert, _create_private_key
 
 
 def init_web():
@@ -77,52 +71,6 @@ def init_self_signed_certs(force: bool):
         cprint("done.", "green")
 
 
-def _create_cert(path: pathlib.Path, pkey: rsa.RSAPrivateKey, crt_name: str, common_name: str):
-    subject = issuer = x509.Name([
-        x509.NameAttribute(NameOID.COUNTRY_NAME, u"CA"),
-        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"Quebec"),
-        x509.NameAttribute(NameOID.LOCALITY_NAME, u"Montreal"),
-        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"C3G McGill"),
-        x509.NameAttribute(NameOID.COMMON_NAME, u"{}".format(common_name))
-    ])
-
-    cert = x509.CertificateBuilder().subject_name(
-        subject
-    ).issuer_name(
-        issuer
-    ).public_key(
-        pkey.public_key()
-    ).serial_number(
-        x509.random_serial_number()
-    ).not_valid_before(
-        datetime.datetime.utcnow()
-    ).not_valid_after(
-        datetime.datetime.utcnow() + datetime.timedelta(days=365)
-    ).add_extension(
-        x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),
-        critical=False
-    ).sign(pkey, hashes.SHA256())
-
-    cert_path = (path / crt_name)
-    with open(cert_path, "wb") as f:
-        f.write(cert.public_bytes(serialization.Encoding.PEM))
-
-
-def _create_private_key(path: pathlib.Path, pkey_name: str):
-    key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048
-    )
-    pkey_path = (path / pkey_name)
-    with open((pkey_path), "wb") as f:
-        f.write(key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.NoEncryption(),
-        ))
-    return key
-
-
 def init_dirs():
     data_dir_vars = {
         "root": "BENTOV2_ROOT_DATA_DIR",
@@ -160,7 +108,7 @@ def init_docker():
 
 
 def init_secrets():
-    # TODO
+    # TODO: init docker-swarm/tmp secrets
     pass
 
 
