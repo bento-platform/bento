8284914c5565768a1dd718f58b7f62ce0a2a4f4f
David Lougheed <david.lougheed@gmail.com>
2023-02-10 13:22:43 -0500

    fix: unset combined services in bentoctl.config

--- py_bentoctl/config.py
+++ py_bentoctl/config.py
@@ -6,6 +6,8 @@ import pathlib
 import shutil
 import yaml
 
+from typing import Tuple
+
 __all__ = [
     "DOCKER_COMPOSE_FILE_BASE",
     "DOCKER_COMPOSE_FILE_DEV",
@@ -35,30 +37,37 @@ USER = os.getenv("USER")
 MODE = os.getenv("MODE")
 DEV_MODE = MODE == "dev"
 
-COMPOSE: tuple[str, ...] = ("docker", "compose")
+COMPOSE: Tuple[str, ...] = ("docker", "compose")
 if shutil.which("docker-compose"):
     COMPOSE = ("docker-compose",)
 
 # Load base docker-compose services list
 with open(DOCKER_COMPOSE_FILE_BASE) as dcf:
     DOCKER_COMPOSE_BASE_DATA = yaml.load(dcf, yaml.Loader)
-    base_services = list(DOCKER_COMPOSE_BASE_DATA["services"].keys())
+
+BASE_SERVICES: Tuple[str, ...] = tuple(DOCKER_COMPOSE_BASE_DATA["services"].keys())
 
 # Load dev docker-compose services list if in DEV_MODE
 if DEV_MODE:
     with open(DOCKER_COMPOSE_FILE_DEV) as dcf_dev:
         DOCKER_COMPOSE_DEV_BASE_DATA = yaml.load(dcf_dev, yaml.Loader)
-    dev_services = list(DOCKER_COMPOSE_DEV_BASE_DATA["services"].keys())
-    dev_services = [service for service in dev_services if service not in base_services]
-    combined_services = base_services + dev_services
-    DOCKER_COMPOSE_DEV_SERVICES = tuple(dev_services)
 
-DOCKER_COMPOSE_SERVICES = tuple(combined_services)
+    DOCKER_COMPOSE_DEV_SERVICES = tuple(
+        service for service in DOCKER_COMPOSE_DEV_BASE_DATA["services"].keys()
+        if service not in BASE_SERVICES)
+
+    DOCKER_COMPOSE_SERVICES = BASE_SERVICES + DOCKER_COMPOSE_DEV_SERVICES
+
+else:
+    DOCKER_COMPOSE_SERVICES = BASE_SERVICES
+
 
 BENTO_SERVICES_PATH = os.getenv("BENTO_SERVICES", pathlib.Path(
     __file__).parent.parent / "etc" / "bento_services.json")
+
 with open(BENTO_SERVICES_PATH, "r") as sf:
     BENTO_SERVICES_DATA = json.load(sf)
+
 
 BENTO_ORCHESTRATION_STATE_DB_FILE = os.getenv(
     "BENTO_ORCHESTRATION_STATE_DB", "./.bentoctl.state.db")
