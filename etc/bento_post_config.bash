13c277d73c43de0e06729fa9d369c7ac9ebc65ce
David Lougheed <david.lougheed@gmail.com>
2024-01-29 13:23:21 -0500

    chore: rm redundant default/overridden-setting

--- etc/bento_post_config.bash
+++ etc/bento_post_config.bash
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 if [[ "$BENTO_GATEWAY_USE_TLS" == 'true' || "$BENTO_GATEWAY_USE_TLS" == '1' ]]; then
-  KC_PROXY='none'
-  KC_HOSTNAME_STRICT='true'
-  KC_HOSTNAME_STRICT_HTTPS='true'
-  KC_HTTP_ENABLED='false'
   KC_HTTPS_CERTIFICATE_FILE=/run/secrets/keycloak-cert-file
   KC_HTTPS_CERTIFICATE_KEY_FILE=/run/secrets/keycloak-cert-key-file
 else
