45f85bb960600edc6be61e781dd3acbbf187d4be
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2024-10-17 13:14:07 -0400

    fix: keycloak ssl termination

--- etc/bento_post_config.bash
+++ etc/bento_post_config.bash
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 if [[ "$BENTO_GATEWAY_USE_TLS" == 'true' || "$BENTO_GATEWAY_USE_TLS" == '1' ]]; then
-  KC_HOSTNAME_STRICT_HTTPS='true'
+  KC_HOSTNAME=${BENTOV2_AUTH_DOMAIN}
   KC_HTTP_ENABLED='false'
   KC_HTTPS_CERTIFICATE_FILE=/run/secrets/keycloak-cert-file
   KC_HTTPS_CERTIFICATE_KEY_FILE=/run/secrets/keycloak-cert-key-file
   KC_PROXY='passthrough'
 else
   # Disable TLS in keycloak
-  KC_HOSTNAME_STRICT_HTTPS='false'
+  KC_HOSTNAME=https://${BENTOV2_AUTH_DOMAIN}  # full URL with HTTPS when KC_HTTP_ENABLED=true
   KC_HTTP_ENABLED='true'
   KC_PROXY='edge'
+  KC_PROXY_HEADERS=xforwarded
 fi
