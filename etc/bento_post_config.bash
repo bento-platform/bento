dc4fb34c818f3e1918cc8b7337250d5a50c960a5
v-rocheleau <victor.rocheleau@mcgill.ca>
2024-10-29 16:36:29 -0400

    chore: remove deprecated keycloak proxy option

--- etc/bento_post_config.bash
+++ etc/bento_post_config.bash
@@ -5,11 +5,9 @@ if [[ "$BENTO_GATEWAY_USE_TLS" == 'true' || "$BENTO_GATEWAY_USE_TLS" == '1' ]]; 
   KC_HTTP_ENABLED='false'
   KC_HTTPS_CERTIFICATE_FILE=/run/secrets/keycloak-cert-file
   KC_HTTPS_CERTIFICATE_KEY_FILE=/run/secrets/keycloak-cert-key-file
-  KC_PROXY='passthrough'
 else
   # Disable TLS in keycloak
   KC_HOSTNAME=https://${BENTOV2_AUTH_DOMAIN}  # full URL with HTTPS when KC_HTTP_ENABLED=true
   KC_HTTP_ENABLED='true'      # Required for TLS termination at the proxy
-  KC_PROXY='edge'
   KC_PROXY_HEADERS=xforwarded # xforwarded (non-standard) instead of forwarded (RFC7239) for NGINX compatibility
 fi
