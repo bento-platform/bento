c7b4c32439b8063a448caa23f5dc9b1276433f5c
David Lougheed <david.lougheed@gmail.com>
2025-10-22 17:42:03 -0400

    work on database dump/wipe/load

--- docs/migrating_to_20.md
+++ docs/migrating_to_20.md
@@ -1,5 +1,32 @@
 # Migrating to Bento v20
 
+TODO
+
 ## 1. Dump all Postgres databases
 
 TODO
+
+```bash
+./bentoctl.bash pg-dump db_dumps/v19_to_v20
+```
+
+This will make `*.pgdump` files in a directory called `v19_to_v20` inside the `db_dumps` folder.
+
+
+## 2. Shut down Bento and pull new images
+
+```bash
+./bentoctl.bash stop
+./bentoctl.bash pull
+```
+
+
+## 3. Remove old database volumes
+
+TODO
+
+```bash
+./bentoctl.bash pg-wipe
+```
+
+All database volume directories will now be empty.
--- py_bentoctl/db_helpers.py
+++ py_bentoctl/db_helpers.py
@@ -1,14 +1,172 @@
-import docker
+import os
+import py_bentoctl.utils as u
+import shutil
+import socket
+
+from dataclasses import dataclass
+from pathlib import Path
+
+from .config import BentoOptionalFeature, BENTO_FEATURE_AUTH
 
 __all__ = [
-    "dump_pg",
-    "load_pg",
+    "pg_dump",
+    "pg_load",
 ]
 
+POSTGRES_DB_CONTAINER_ENV_PREFIXES_AND_FEATURES: tuple[tuple[str, BentoOptionalFeature | None], ...] = (
+    ("BENTO_AUTH_DB", BENTO_FEATURE_AUTH),
+    ("BENTO_AUTHZ_DB", None),
+    ("BENTO_REFERENCE_DB", None),
+    ("BENTOV2_KATSU_DB", None),
+)
 
-def dump_pg():
-    pass
+
+@dataclass
+class PostgresContainer:
+    container: str
+    image: str
+    version: str
+    # -----------
+    vol_dir: Path
+    # -----------
+    db: str
+    username: str
+    # password: str
 
 
-def load_pg():
-    pass
+def process_ansi(res: bytes) -> bytes:
+    # noinspection PyTypeChecker
+    return res.replace(b"\x00", b"").replace(b"\x01", b"").replace(b"\x02", b"").lstrip(b"]k")
+
+
+def _load_pg_db_containers() -> list[PostgresContainer]:
+    pg_containers: list[PostgresContainer] = []
+
+    for prefix, feature in POSTGRES_DB_CONTAINER_ENV_PREFIXES_AND_FEATURES:
+        if feature is not None and not feature.enabled:
+            continue
+
+        pg_containers.append(
+            PostgresContainer(
+                container=os.getenv(f"{prefix}_CONTAINER_NAME"),
+                image=os.getenv(f"{prefix}_IMAGE"),
+                version=os.getenv(f"{prefix}_VERSION"),
+                vol_dir=Path(os.getenv(f"{prefix}_VOL_DIR")),   # TODO: this is wrong for katsu
+                db=os.getenv(f"{prefix}_NAME"),
+                username=os.getenv(f"{prefix}_USER"),
+            )
+        )
+
+    return pg_containers
+
+
+def pg_dump(pgdump_dir: Path):
+    docker_client = u.get_docker_client()
+    pg_containers = _load_pg_db_containers()
+
+    for pg in pg_containers:
+        u.info(f"creating pgdumps for database {pg.container}")
+
+        container = docker_client.containers.get(pg.container)
+
+        # ---
+
+        _, output = container.exec_run(("pg_dumpall", "-U", pg.username), stream=True, demux=True)
+
+        # ---
+
+        if not pgdump_dir.exists():
+            pgdump_dir.mkdir(parents=True, exist_ok=True)
+
+        # TODO: validate not empty
+
+        with open(pgdump_dir / f"{pg.container}.pgdump", "wb") as fh:
+            for chunk_stdout, chunk_stderr in output:
+                if chunk_stderr:
+                    u.warn(chunk_stderr.decode("ascii"))
+                if chunk_stdout:
+                    fh.write(chunk_stdout)
+
+    # TODO: write success
+
+
+def pg_load(pgdump_dir: Path):
+    if not pgdump_dir.exists():
+        u.err(f"directory {pgdump_dir} does not exist")
+        exit(1)
+
+    docker_client = u.get_docker_client()
+    pg_containers = _load_pg_db_containers()
+
+    for pg in pg_containers:
+        container = docker_client.containers.get(pg.container)
+
+        u.info(f"working on postgres database {pg.container}")
+
+        container_pgdump_to_load = pgdump_dir / f"{pg.container}.pgdump"
+
+        if not container_pgdump_to_load.exists():
+            u.err(f"    file {container_pgdump_to_load} does not exist")
+            # TODO
+            exit(1)
+
+        # --------------------------------------------------------------------------------------------------------------
+
+        u.task_print(f"    importing {container_pgdump_to_load} into container...")
+
+        skt: socket.SocketIO
+        _, skt = container.exec_run(rf"psql -U {pg.username} {pg.db}", stdin=True, socket=True)
+        # noinspection PyUnresolvedReferences,PyProtectedMember
+        s: socket.socket = skt._sock
+
+        # Receive any initial messages
+
+        s.settimeout(1.0)
+        try:
+            initial_messages: bytes = s.recv(1024 * 100)
+            u.warn("    receieved initial messages:")
+            u.warn(u.indent_str(process_ansi(initial_messages).decode("ascii"), 6))
+        except TimeoutError:
+            pass
+        s.settimeout(None)
+
+        with open(container_pgdump_to_load, "rb") as fh:
+            while send_chunk := fh.read(4096):
+                # TODO:
+                # s.sendall(send_chunk)
+                pass
+
+        # noinspection PyTypeChecker
+        s.sendall(b"SELECT * FROM dasdf;\n")
+        response: bytes = process_ansi(s.recv(1024 * 100))
+
+        # noinspection PyTypeChecker
+        has_error = response.find(b"ERROR:") >= 0
+        if has_error:
+            u.task_print_done("failed.", color="red")
+            u.err(u.indent_str(response.decode("ascii").strip(), 6))
+            # exit(1)
+        else:
+            u.task_print_done("imported.")
+
+
+def pg_wipe():  # TODO: hook up to cli
+    """
+    Scary utility!! Wipes all Postgres database volumes.
+    """
+
+    confirm = input("Are you sure you want to wipe all Postgres database volumes? [y/N]")
+
+    if confirm.lower() != "y":
+        u.info("terminating without destroying data.")
+        exit(0)
+
+    # Now we do the scary thing
+
+    for pg in _load_pg_db_containers():
+        u.task_print(f"deleting {pg.container} database volume...")
+        if not pg.vol_dir.exists():
+            u.task_print_done("already does not exist.", color="yellow")
+            continue
+        shutil.rmtree(pg.vol_dir)
+        pg.vol_dir.mkdir(exist_ok=True)
